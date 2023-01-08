873996f247d2dfea9f1586e1798b8d4f63168a8a
iamabdh <abdullahalbadri09@gmail.com>
2023-01-08 10:26:28 +0400

    add: test number for power function

--- src/main/java/io/code/CodeExample.java
+++ src/main/java/io/code/CodeExample.java
@@ -1,12 +1,28 @@
 package io.code;
 
+import java.util.HashMap;
+
 public class CodeExample {
 	
 	public static void main(String[] arg) {
-		
 	}
 	
 	public int DummyFunction(int a) {
 		return a;
+	}
+	
+	public  HashMap<String, Integer> POW(int a, int b) {
+		HashMap<String, Integer> outpHashMap = new HashMap<String, Integer>();
+		outpHashMap.put("output", a);
+		int g = a;
+		if (b == 0) {
+			a = 1;
+		} else  {
+			for (int i  = 1; i < b; i ++) {
+				a *= g;
+			}
+		}
+		outpHashMap.put("output", a);
+		return outpHashMap;
 	}
 }
--- src/test/java/io/code/CodeExampleTest.java
+++ src/test/java/io/code/CodeExampleTest.java
@@ -1,15 +1,41 @@
 package io.code;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import java.util.HashMap;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
 
 public class CodeExampleTest {
 
 	@Test
-	public void dummyTest() {
+	@DisplayName("Postive number")
+	public void POWTestCase1() {
 		CodeExample cExample = new CodeExample();
-		assertEquals(1, cExample.DummyFunction(1));
+		HashMap<String, Integer> resultHashMap = new HashMap<String, Integer>();
+		resultHashMap.put("output", 9);
+		assertEquals(resultHashMap, cExample.POW(3, 2));
+	}
+	
+	@Test
+	@DisplayName("Negtive number")
+	public void POWTestCase2() {
+		CodeExample cExample = new CodeExample();
+		HashMap<String, Integer> resultHashMap = new HashMap<String, Integer>();
+		resultHashMap.put("output", 9);
+		assertEquals(resultHashMap, cExample.POW(-3, 2));
+	}
+	
+	@Test
+	@DisplayName("Zero number")
+	public void POWTestCase3() {
+		CodeExample cExample = new CodeExample();
+		HashMap<String, Integer> resultHashMap = new HashMap<String, Integer>();
+		resultHashMap.put("output", 1);
+		assertEquals(resultHashMap, cExample.POW(0, 0));
 	}
 
 }
