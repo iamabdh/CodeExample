a00a853f785b423e2c984150e29c8eba63735646
iamabdh <abdullahalbadri09@gmail.com>
2023-01-08 10:10:01 +0400

    add: test unit for odd even number

--- src/test/java/io/code/CodeExampleTest.java
+++ src/test/java/io/code/CodeExampleTest.java
@@ -2,14 +2,27 @@ package io.code;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.junit.Test;
 
 public class CodeExampleTest {
-
+	
+	
 	@Test
-	public void dummyTest() {
+	public void OddEvenTest() {
 		CodeExample cExample = new CodeExample();
-		assertEquals(1, cExample.DummyFunction(1));
+		HashMap<String, ArrayList<Integer>> resultHashMap = new HashMap<String, ArrayList<Integer>>();
+		ArrayList<Integer> oddNumbersArrayList = new ArrayList<Integer>();
+		ArrayList<Integer> evenNumbersArrayList = new ArrayList<Integer>();
+		oddNumbersArrayList.add(1);
+		oddNumbersArrayList.add(3);
+		evenNumbersArrayList.add(4);
+		evenNumbersArrayList.add(6);
+		resultHashMap.put("even", evenNumbersArrayList);
+		resultHashMap.put("odd", oddNumbersArrayList);
+		assertEquals(resultHashMap, cExample.OddEvenNumber(new int[] {1, 3, 4, 6}));
 	}
 
 }
