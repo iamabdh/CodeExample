a578b0333a18ad9b7991fa644bce8f067ef0dc91
iamabdh <abdullahalbadri09@gmail.com>
2023-01-08 10:55:46 +0400

    add: test unit for reverse string

--- src/main/java/io/code/CodeExample.java
+++ src/main/java/io/code/CodeExample.java
@@ -1,12 +1,31 @@
 package io.code;
 
+import java.util.ArrayList;
+
 public class CodeExample {
 	
 	public static void main(String[] arg) {
-		
 	}
 	
 	public int DummyFunction(int a) {
 		return a;
+	}
+	
+	public  String reverseString(String str) {
+		ArrayList<Character> strCharsArrayList = new ArrayList<Character>();
+		String newString = new String();
+		try {
+			for (int i = str.length() - 1; i >= 0; i--) {
+				strCharsArrayList.add(str.charAt(i));
+			}
+			
+			for (int i = 0; i < strCharsArrayList.size(); i ++) {
+				newString += strCharsArrayList.get(i);
+			}
+				
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return newString;
 	}
 }
--- src/test/java/io/code/CodeExampleTest.java
+++ src/test/java/io/code/CodeExampleTest.java
@@ -2,14 +2,38 @@ package io.code;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 
 public class CodeExampleTest {
 
+
 	@Test
-	public void dummyTest() {
+	@DisplayName("String as argument")
+	public void ReveseStringTestCase1() {
 		CodeExample cExample = new CodeExample();
-		assertEquals(1, cExample.DummyFunction(1));
+		assertEquals("ereht ih", cExample.reverseString("hi there"));
+	}
+	
+	@Test
+	@DisplayName("Null as argument")
+	public void ReveseStringTestCase2() {
+		CodeExample cExample = new CodeExample();
+		assertEquals("", cExample.reverseString(null));
+	}
+	
+	@Test
+	@DisplayName("Empty String as argument")
+	public void ReveseStringTestCase3() {
+		CodeExample cExample = new CodeExample();
+		assertEquals("", cExample.reverseString(""));
+	}
+	@Test
+	@DisplayName("Single Char as argument")
+	public void ReveseStringTestCase4() {
+		CodeExample cExample = new CodeExample();
+		assertEquals("a", cExample.reverseString("a"));
 	}
 
 }
