904ef0e25fe3839041ded863f1728bea3b03a60c
iamabdh <abdullahalbadri09@gmail.com>
2023-01-10 10:08:32 +0400

    add: checking valid statement test unit

--- src/main/java/io/code/CodeExample.java
+++ src/main/java/io/code/CodeExample.java
@@ -1,12 +1,49 @@
 package io.code;
 
+import java.util.Stack;
+
 public class CodeExample {
 	
 	public static void main(String[] arg) {
+		System.out.println(checkingValidStatementBalance(null));
 		
 	}
 	
-	public int DummyFunction(int a) {
-		return a;
+	public static boolean checkingValidStatementBalance(String str) {
+		Stack<Character> stack = new Stack<Character>();
+		int i = 0;
+		try {
+			while (i < str.length()) {
+				if (str.charAt(i) == '{' || str.charAt(i) == '(' || str.charAt(i) == '[') {
+					stack.push(str.charAt(i));
+				} else if (str.charAt(i) == '}') {
+					if (!stack.isEmpty() && stack.peek() == '{') {
+						stack.pop();	
+					}
+					else {
+						stack.push(str.charAt(i));
+					}
+				} else if (str.charAt(i) == ')') {	
+					if (!stack.isEmpty() && stack.peek() == '(') {
+						stack.pop();	
+					}
+					else {
+						stack.push(str.charAt(i));
+					}
+				} else if (str.charAt(i) == ']') {	
+						if (!stack.isEmpty() && stack.peek() == '[') {
+							stack.pop();	
+						}
+						else {
+							stack.push(str.charAt(i));
+						}
+					} 
+				i ++;
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		return stack.isEmpty();
 	}
 }
--- src/test/java/io/code/CodeExampleTest.java
+++ src/test/java/io/code/CodeExampleTest.java
@@ -1,15 +1,24 @@
 package io.code;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class CodeExampleTest {
 
 	@Test
-	public void dummyTest() {
+	@DisplayName("Take null as an arguments.")
+	void CheckValidStatementTestCase1() {
 		CodeExample cExample = new CodeExample();
-		assertEquals(1, cExample.DummyFunction(1));
+		assertEquals(true, cExample.checkingValidStatementBalance(null));
+	}
+	
+	@Test
+	@DisplayName("Take an empty string as an argument.")
+	void CheckValidStatementTestCase2() {
+		CodeExample cExample = new CodeExample();
+		assertEquals(true, cExample.checkingValidStatementBalance("  "));
 	}
 
 }
